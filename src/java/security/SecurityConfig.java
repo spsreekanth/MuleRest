dafa3ec35ee5034354ab66cc13fb3172f9d93a8a
spsreekanth <spsreekanth@users.noreply.github.com>
2023-02-07 21:30:42 -0600

    Create SecurityConfig.java

---
+++ src/java/security/SecurityConfig.java
@@ -0,0 +1,11 @@
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/proxy/**").authenticated()
+                .and()
+                .
